00e8910f5a55fcac8fa103cacebf2147afd25695
Dimitrios Savvopoulos <ds@dimsav.com>
2014-12-09 13:45:58 +0100

    Added dummy seeds for development.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use Dimsav\Todo\Models\User;
+use Dimsav\Todo\Models\Todo;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -11,7 +14,43 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		DB::table('users')->delete();
+
+		User::create([
+			'id' => 1,
+			'email' => 'dimsav@madewithlove.be',
+			'password' => Hash::make('secret'),
+		]);
+
+		DB::table('todos')->delete();
+
+		$todos = [
+			[
+				'text' => 'Apply to madewithlove',
+				'finished' => true,
+				'user_id' => 1,
+			],
+			[
+				'text' => 'Start assignment',
+				'finished' => true,
+				'user_id' => 1,
+			],
+			[
+				'text' => 'Finish assignment',
+				'finished' => false,
+				'user_id' => 1,
+			],
+			[
+				'text' => 'Send assignment',
+				'finished' => false,
+				'user_id' => 1,
+			],
+		];
+		foreach ($todos as $todo)
+		{
+			Todo::create($todo);
+		}
+
 	}
 
 }
