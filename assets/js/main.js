b83e1fb4c8899c36332f77fe0b98f3bbde5a6b36
Dimitrios Savvopoulos <ds@dimsav.com>
2014-12-09 20:14:07 +0100

    Added navigation with arrow keys between tasks with jQuery.

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    $('table.todos').on('keypress', 'input.text', function(e)
+    {
+        if (e.keyCode == 40)
+        {
+            var $next = $(e.currentTarget).closest('tr').next().find('input.text');
+            if ($next.length)
+            {
+                $next.focus();
+            }
+        }
+        else if (e.keyCode == 38)
+        {
+            var $previous = $(e.currentTarget).closest('tr').prev().find('input.text');
+            if ($previous.length)
+            {
+                $previous.focus();
+            }
+        }
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,13 @@ var cssFiles = [
 
 var jsFiles = [
     'assets/vendor/modernizr/modernizr.js',
+    'assets/vendor/jquery/dist/jquery.js',
     'assets/vendor/angular/angular.js',
     'assets/vendor/angular-resource/angular-resource.js',
     'assets/js/app.js',
     'assets/js/services.js',
-    'assets/js/controllers.js'
+    'assets/js/controllers.js',
+    'assets/js/main.js'
 ];
 
 var imgFiles = [
